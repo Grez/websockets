e44bcedb692d60514bc066418f25375a5a9139a7
Tom Barton <grez.cz@gmail.com>
2016-04-03 13:36:29 +0200

    Updated JS

--- js/websockets.js
+++ js/websockets.js
@@ -4,14 +4,28 @@
 
 		init: function () {
 			var self = this;
-			var userId = Cookies.get('teddy.userId');
-			var apiKey = Cookies.get('teddy.apiKey');
+			self.userId = Cookies.get('teddy.userId');
+			self.apiKey = Cookies.get('teddy.apiKey');
+			self.makeConnection();
+		}
 
+	}, {
+
+		conn: false,
+		attempt: 0,
+		userId: '',
+		apiKey: '',
+		intervals: [1, 2, 5, 30, 120, 300], // how often should we try to reconnect
+
+		makeConnection: function () {
+			var self = this;
 			self.conn = new WebSocket('ws://localhost:8080');
+			self.attempt++;
 
 			self.conn.onopen = function(e) {
+				self.attempt = 0; // successful connection, let's set attempts back to 0
+				self.authorize(self.userId, self.apiKey);
 				console.log("Connection established!");
-				self.authorize(userId, apiKey);
 			};
 
 			self.conn.onerror = function(e) {
@@ -20,7 +34,18 @@
 			};
 
 			self.conn.onclose = function(e) {
-				console.log("Terminated");
+				console.log('Connection terminated. Attempt: #' + self.attempt);
+				console.log(e);
+
+				if (self.attempt >= self.intervals.length) {
+					console.log('We\'ve tried to reconnect too many times');
+					return;
+				}
+
+				console.log('Waiting for ' + self.intervals[self.attempt] + ' s and going to try again');
+				setTimeout(function () {
+					self.makeConnection();
+				}, self.intervals[self.attempt] * 1000);
 			};
 
 			self.conn.onmessage = function(e) {
@@ -29,11 +54,8 @@
 					events.updateUnreadMessages(msg.data);
 				}
 			};
-		}
+		},
 
-	}, {
-
-		conn: false,
 		msg: function (method, msg) {
 			var req = {
 				method: method,
@@ -42,10 +64,11 @@
 			var json = JSON.stringify(req, null, 2);
 			this.conn.send(json);
 		},
+
 		authorize: function (userId, apiKey) {
 			var msg = { userId: userId, apiKey: apiKey };
 			this.msg('authorize', msg);
-		}
+		},
 
 	});
 
